c5d190b47bda1838728bd6ac9ef11de3441a955e
kbuczek <krzysztofbuczek@live.com>
2024-12-16 23:22:35 +0100

    working GitHub signIn signOut auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
-import { AuthScreen } from '@/features/auth/components/AuthScreen';
+import { useAuthActions } from '@convex-dev/auth/react';
 
 export default function Home() {
+  const { signOut } = useAuthActions();
+
   return (
-    <>
-      <AuthScreen />
-    </>
+    <div>
+      Logged in! <Button onClick={() => signOut()}>Sign out</Button>
+    </div>
   );
 }
